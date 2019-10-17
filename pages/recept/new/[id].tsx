ff0492ce7bd598e6a5e0c275fe97117d508e3187
Joel Gullander <jag@joelgullander.se>
2019-10-18 00:53:07 +0200

    mmm

---
+++ pages/recept/new/[id].tsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import { NextPage, NextPageContext } from 'next';
+import { Recipe, Keyword } from '../../../types/Recipe';
+import Container from '../../../components/Container';
+import { Select, Input, TimePicker, Button } from 'antd';
+import axios from 'axios';
+import moment from 'moment';
+
+const { Option } = Select;
+const { TextArea } = Input;
+const format = 'HH:mm';
+
+
+interface MyPropsInterface {
+    recipe: Recipe,
+}
+
+interface IState {
+    recipe: Recipe
+}
+
+interface Context extends NextPageContext {
+
+} 
+
+interface ServerResponse {
+    data: Recipe
+}
+
+
+class NewRecipePage extends React.Component<MyPropsInterface, IState> {
+    state = {
+        recipe: this.props.recipe
+    }
+    static async getInitialProps({ query }: NextPageContext) {
+        const recipe : ServerResponse = await axios.get(`http://localhost:5000/api/v1/Recipe/${query.id}`);
+        return {
+            recipe: recipe.data
+        }
+    }
+
+    fieldsOnChange = (event) => {
+        const path = event.target.name.split('.');
+        const depth = path.length;
+        const state = { ...this.state };
+        let stateRef = state;
+        for (let i = 0; i < depth; i += 1) {
+          if (i === depth - 1) {
+            stateRef[path[i]] = event.target.value;
+          } else {
+            stateRef = stateRef[path[i]];
+          }
+        }
+        this.setState(state);
+    }
+
+    cookingTimeChange = (time, timeString) => {
+        this.setState({
+            recipe: {
+                ...this.state.recipe,
+                cookingTime: timeString
+            }
+        })
+    }
+
+    submitRecipe = () => {
+        axios.patch(`/api/v1/Recipe/${this.state.recipe.recipeId}`, {
+            ...this.state.recipe
+        })
+    }
+  
+    render() {
+        // const { keywords } = this.state;
+        // const children = keywords.map(keyword => <Option key={keyword.keywordName}>{keyword.keywordName}</Option>);
+        const portions = [1,2,3,4,5,6,7,8,9,10,11,12].map(key => <Option key={key}>{key} portioner </Option>);
+        console.log(this.state.recipe)
+        return (
+            <div className="p-newrecipe">
+                <Container>
+                    <h2>Nytt recept:</h2>
+                    <div className="p-newrecipe__divider">
+                        <Input name="recipe.name" onChange={this.fieldsOnChange} defaultValue={this.props.recipe.name} placeholder="Receptnamn..." />
+                    </div>
+                    <div className="p-newrecipe__divider">
+                        <TextArea name="recipe.description" placeholder="Testdescription" defaultValue={this.props.recipe.description} autosize={{minRows: 15}} onChange={this.fieldsOnChange} />
+                    </div>
+                    <div className="p-newrecipe__divider">
+                        <TextArea name="recipe.recipeIngredients" placeholder="Ingredienser"  defaultValue={this.props.recipe.recipeIngredients} autosize={{minRows: 15}} onChange={this.fieldsOnChange} />
+                    </div>
+                    <div className="p-newrecipe__divider">
+                        <TextArea name="recipe.recipeInstructions" placeholder="Instruktioner" defaultValue={this.props.recipe.recipeInstructions} autosize={{minRows: 15}} onChange={this.fieldsOnChange} />
+                    </div>
+                    <div className="p-newrecipe__divider">
+                        <TimePicker placeholder="Tillagningstid" style={{marginRight: '10px'}} onChange={this.cookingTimeChange} format={format} />
+                        <Select defaultValue="4" placeholder="Keywords">
+                            {portions}
+                        </Select>
+                    </div>
+                    <div className="p-newrecipe__divider">
+                        {/* Keywords:
+                        <Select mode="tags" style={{ width: '100%' }} placeholder="Keywords" onDeselect={this.handleDeselect} onSearch={this.handleSearch} onSelect={this.handleSelect}>
+                            {children}
+                        </Select> */}
+                    </div>
+
+                    <div className="p-newrecipe__divider">
+      
+                    </div>
+
+                <Button onClick={this.submitRecipe}>Spara recept</Button>
+                </Container>
+                <style jsx>{`
+                    .p-newrecipe {
+                        padding: 20px 0;
+                    }
+                    .p-newrecipe__divider {
+                        margin-bottom: 20px;
+                    }
+                `}</style>
+            </div>
+        )
+    }
+  }
+
+export default NewRecipePage;
--- types/Recipe.tsx
+++ types/Recipe.tsx
@@ -9,19 +9,25 @@ export interface Cuisine {
     cuisineName: string
 }
 
+export interface Keyword {
+    keywordId: number,
+    keywordName: string,
+    RecipeKeywords?: Array<object> 
+}
+
 export interface Recipe {
-    recipeId: Number,
-    created: Date,
-    updated: Date,
-    name: string,
-    description: string,
-    author: string,
-    cookingTime: Date,
-    prepTime: Date,
-    recipeKeywords?: Array<object>,
-    recipeCategory: Category,
-    recipeCuisine: Cuisine,
-    recipeIngredients: string,
-    recipeInstructions: string
-    quantityServings: number
+    recipeId?: Number | null,
+    created: string | null,
+    updated: string | null,
+    name: string | null,
+    description: string | null,
+    author: string | null,
+    cookingTime: string | null,
+    prepTime: string | null,
+    recipeKeywords?: Array<object> | null,
+    recipeCategory: Category | null,
+    recipeCuisine: Cuisine | null,
+    recipeIngredients: string | null,
+    recipeInstructions: string | null,
+    quantityServings: number | null
   }
